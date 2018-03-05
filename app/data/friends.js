c3a0dc465e2f690302c5c0c9dbc84ffff2df32da
RachetRalph <ralphandrade@zoho.com>
2018-03-05 11:17:36 -0700

    Deleted node modules from repo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,54 +1,150 @@
 // Friends array of objects for possible matches  
-const friendList = [{
-    name: "Dimitri",
-    photo: "https://static.pexels.com/photos/326559/pexels-photo-326559.jpeg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-},
-{
-    name: "Jasmine",
-    photo: "https://www.instagram.com/p/BeAObOwHRid/?taken-by=pexelsphotos",
-    scores: [1, 3, 4, 2, 5, 1, 2, 3, 4, 5]
-},
-{
-    name: "Rudy",
-    photo: "https://static.pexels.com/photos/21278/pexels-photo.jpg",
-    scores: [5, 4, 1, 2, 3, 4, 5, 4, 3, 2]
-},
-{
-    name: "Albert",
-    photo: "https://static.pexels.com/photos/567459/pexels-photo-567459.jpeg",
-    scores: [2, 3, 4, 5, 5, 4, 3, 2, 1, 5]
-},
-{
-    name: "Laine",
-    photo: "https://static.pexels.com/photos/573299/pexels-photo-573299.jpeg",
-    scores: [1, 2, 4, 4, 5, 3, 2, 1, 4, 2]
-},
-{
-    name: "Carmen",
-    photo: "https://images.pexels.com/photos/253758/pexels-photo-253758.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
-    scores: [1, 3, 4, 2, 5, 1, 2, 3, 4, 5]
-},
-{
-    name: "Eddie",
-    photo: "https://static.pexels.com/photos/683381/pexels-photo-683381.jpeg",
-    scores: [2, 3, 5, 2, 2, 1, 3, 3, 3, 4]
-},
-{
-    name: "Jasmine",
-    photo: "https://static.pexels.com/photos/413727/pexels-photo-413727.jpeg",
-    scores: [1, 2, 4, 4, 5, 2, 2, 5, 4, 2]
-},
-{
-    name: "Josephine",
-    photo: "https://static.pexels.com/photos/227335/pexels-photo-227335.jpeg",
-    scores: [1, 2, 5, 2, 1, 1, 4, 3, 1, 1]
-},    
-{
-    name: "Victor",
-    photo: "https://static.pexels.com/photos/247917/pexels-photo-247917.jpeg",
-    scores: [5, 3, 2, 2, 1, 1, 3, 3, 2, 5]
-}
+const friendList = [
+    {
+        name: "Dimitri",
+        photo: "http://hairzstyle.com/wp-content/uploads/2016/08/1-Best-Men-Haircuts-Ideas-2.jpg",
+        scores: [
+            5, 
+            1, 
+            4, 
+            4, 
+            5, 
+            1, 
+            2, 
+            5, 
+            4, 
+            1
+        ]
+    },
+    {
+        name: "Jasmine",
+        photo: "https://data.whicdn.com/images/163497674/large.jpg",
+        scores: [
+            1, 
+            3, 
+            4, 
+            2, 
+            5, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5
+        ]
+    },
+    {
+        name: "Rudy",
+        photo: "http://hairzstyle.com/wp-content/uploads/2016/08/Best-Men-Haircuts-Ideas-6.jpg",
+        scores: [
+            5,
+            4, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5, 
+            4, 
+            3, 
+            2
+        ]
+    },
+    {
+        name: "Albert",
+        photo: "http://www.haircutsphotos.com/wp-content/gallery/mens-short-haircut/mens-short-haircut-4.jpg",
+        scores: [
+            2, 
+            3, 
+            4, 
+            5, 
+            5, 
+            4, 
+            3, 
+            2, 
+            1, 
+            5
+        ]
+    },
+    {
+        name: "Laine",
+        photo: "https://timedotcom.files.wordpress.com/2014/03/screen-shot-2014-03-24-at-10-23-50-am.png",
+        scores: [
+            1, 
+            2, 
+            4, 
+            4, 
+            5, 
+            3, 
+            2, 
+            1, 
+            4, 
+            2
+        ]
+    },
+    {
+        name: "Carmen",
+        photo: "https://s-media-cache-ak0.pinimg.com/736x/ea/56/e7/ea56e7d33a853b7752e07aa02e71fdba.jpg",
+        scores: [
+            1, 
+            3, 
+            4, 
+            2, 
+            5, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5
+        ]
+    },
+    {
+        name: "Eddie",
+        photo: "https://www.hairstylearchives.com/wp-content/uploads/2015/04/Straight-Hairstyles-For-Men.jpg",
+        scores: [
+            2, 
+            3, 
+            5, 
+            2, 
+            2, 
+            1, 
+            3, 
+            3, 
+            3, 
+            4
+        ]
+
+    },
+    {
+        name: "Josephine",
+        photo: "https://images.techhive.com/images/article/2015/05/thelist-selfies-pet-selfie2-100584124-large.jpg",
+        scores: [
+            1, 
+            2, 
+            5, 
+            2, 
+            1, 
+            2, 
+            4, 
+            3, 
+            1, 
+            1
+        ]
+    },    
+    {
+        name: "Victor",
+        photo: "http://hairzstyle.com/wp-content/uploads/2016/08/Best-Men-Haircuts-Ideas-3.jpg",
+        scores: [
+            5, 
+            3, 
+            2, 
+            2, 
+            1, 
+            1, 
+            3, 
+            3, 
+            2, 
+            5
+        ]
+    }
 ];
 
 module.exports = friendList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 console.log('API Route Connected Successfully');
 
-// Pull in required dependencies
-const path = require('path');
 // Import the list of friend entries
 const friendList = require('../data/friends.js');
 
@@ -10,24 +8,26 @@ module.exports = (app) => {
     // A GET route that displays all friends in a JSON format
     app.get("/api/friends", function(req,res){
         res.json(friendList);
+        
     }); // End of GET route
 
     // A POST route to handle incoming survey results & compatabilty logic
     app.post("/api/friends", function(req,res){
-
-        let newHomieScores = req.body.scores;
+        let newHomieScores = req.body;
         let scoreArr = [];
         let homieCount = 0;
         let bestie = 0;
-
+        console.log(newHomieScores);
+        console.log(bestie);
         for(let i=0; i < friendList.length; i++){
             let scoreDiffr = 0;
 
-        for(let r=0; r < newHomieScores.length; r++){
+        for(var r=0; r < newHomieScores.length; r++){
             scoreDiffr += (Math.abs(parseInt(friendList[i].scores[r]) - parseInt(newHomieScores[r])));
         }
         // Pushing results into scoreArray 
         scoreArr.push(scoreDiffr);
+        // console.log(scoreDiffr);
         }
         
         // Finding best match after comparison 
@@ -39,10 +39,11 @@ module.exports = (app) => {
         //  return bestie data
         let bff = friendList[bestie];
         res.json(bff);
-
+        console.log(bff);
         // pushes new submission into FriendsList array 
         friendList.push(req.body);
-
+        console.log(req.body);
+        
 
     });   // End of post route
 
